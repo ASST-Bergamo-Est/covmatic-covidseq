dee132a4a7f13baff62d3cb899688152818ac333
Agostino Facotti <agostino.facotti@asst-bergamoest.it>
2023-02-02 12:23:52 +0100

    added messages as for covmatic-stations v2.19.5; module name refactor

---
+++ src/covmatic_covidseq/stations/reagent.py
@@ -0,0 +1,48 @@
+from typing import Tuple
+from ..station import CovidseqBaseStation, instrument_loader, labware_loader
+
+
+class ReagentStation(CovidseqBaseStation):
+    def __init__(self,
+                 ot_name = "OT2",
+                 tipracks300_slots: Tuple[str, ...] = ("9",),
+                 tipracks1000_slots: Tuple[str, ...] = ("8",),
+                 *args, **kwargs):
+        super().__init__(ot_name=ot_name, *args, **kwargs)
+        self._tipracks300_slots = tipracks300_slots
+        self._tipracks1000_slots = tipracks1000_slots
+
+    @labware_loader(0, "_tipracks300")
+    def load_tipracks300(self):
+        self._tipracks300 = [
+            self._ctx.load_labware('opentrons_96_filtertiprack_300ul', slot, '300ul filter tiprack')
+            for slot in self._tipracks300_slots]
+
+    @labware_loader(0, "_tipracks1000")
+    def load_tipracks1000(self):
+        self._tipracks1000 = [
+            self._ctx.load_labware('opentrons_96_filtertiprack_1000ul', slot, '1000ul filter tiprack')
+            for slot in self._tipracks1000_slots]
+
+    @instrument_loader(0, '_p300')
+    def load_p300(self):
+        self._p300 = self._ctx.load_instrument('p300_single_gen2', 'right', tip_racks=self._tipracks300)
+
+    @instrument_loader(0, '_p1000')
+    def load_p1000(self):
+        self._p1000 = self._ctx.load_instrument('p1000_single_gen2', 'left', tip_racks=self._tipracks1000)
+
+    def _tipracks(self) -> dict:
+        return {
+            '_tipracks300': '_p300',
+            '_tipracks1000': '_p1000'
+        }
+
+    def body(self):
+        # self.robot_pick_plate("SLOT1", "REAGENT_PLATE")
+        print("HELLO!")
+        pass
+
+
+if __name__ == "__main__":
+    ReagentStation(num_samples=96, metadata={'apiLevel': '2.7'}).simulate()
