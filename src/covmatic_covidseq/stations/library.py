82b4e0839f125d0e77a93fb734ea1dc442d3141e
Agostino Facotti <agostino.facotti@asst-bergamoest.it>
2023-02-22 13:05:25 +0100

    LibraryStation: added transfer sample function

--- src/covmatic_covidseq/stations/library.py
+++ src/covmatic_covidseq/stations/library.py
@@ -1,12 +1,57 @@
 import math
+from itertools import repeat, cycle, islice
 from typing import Tuple
 
 from covmatic_stations.utils import WellWithVolume, MoveWithSpeed
+from opentrons.protocol_api.labware import Well
+from opentrons.types import Point
 
 from ..utils import get_labware_json_from_filename
 from ..pipette_chooser import PipetteChooser
 from ..station import CovidseqBaseStation, labware_loader, instrument_loader
 from covmatic_stations.multi_tube_source import MultiTubeSource
+
+
+def mix_well(pipette,
+             well: Well,
+             volume,
+             repetitions,
+             last_dispense_flow_rate=None,
+             min_z_difference=1.0,
+             travel_speed=25.0):
+    """ Mix a well
+        :param pipette: pipette object to use
+        :param well: well to mix
+        :param volume: volume to mix
+        :param repetitions: number of mix (aspirate-dispense) cycles
+        :param last_dispense_flow_rate: flow rate to use in the last dispense to avoid leaving liquid in tip
+        :param min_z_difference: the minimum difference to have in the vertical axis
+        :param travel_speed: the speed between different positions.
+    """
+    well_with_volume = WellWithVolume(well)
+    height_min = well_with_volume.height
+
+    well_with_volume.fill(volume)
+    height_max = max(well_with_volume.height, height_min + min_z_difference)
+
+    side_movement = (well.diameter or well.length) / 2 * 0.7
+
+    aspirate_pos = [well.bottom((height_min + height_max)/2)]
+    dispense_heights = [height_max, (height_min + height_max)/2,  height_min]
+    dispense_xy_directions = [(1, 0), (0, 1), (-1, 0),  (0, -1),  (1, -1),  (1, 1), (-1, +1), (-1, -1)]
+
+    dispense_pos = [well.bottom(h).move(Point(x=x_side*side_movement, y=y_side*side_movement))
+                    for h, (x_side, y_side) in islice(zip(cycle(dispense_heights),
+                                                      dispense_xy_directions), repetitions)]
+
+    for i, (a, d) in enumerate(zip(cycle(aspirate_pos), dispense_pos)):
+        if i == (repetitions - 1) and last_dispense_flow_rate is not None:
+            pipette.flow_rate.dispense = last_dispense_flow_rate
+        pipette.move_to(a, speed=travel_speed, publish=False)
+        pipette.aspirate(volume)
+        pipette.move_to(d, speed=travel_speed, publish=False)
+        pipette.dispense(volume)
+
 
 class LibraryStation(CovidseqBaseStation):
     def __init__(self,
@@ -17,6 +62,7 @@ class LibraryStation(CovidseqBaseStation):
                  reagent_plate_slot=6,
                  work_plate_slot=5,
                  magdeck_slot=11,
+                 pcr_plate_bottom_height=0.5,
                  *args, **kwargs):
         super().__init__(ot_name=ot_name, *args, **kwargs)
         self._pipette_chooser = PipetteChooser()
@@ -24,6 +70,7 @@ class LibraryStation(CovidseqBaseStation):
         self._reagent_plate_slot = reagent_plate_slot
         self._work_plate_slot = work_plate_slot
         self._magdeck_slot = magdeck_slot
+        self._pcr_plate_bottom_height = pcr_plate_bottom_height
         self._tipracks20_slots = tipracks20_slots
         self._tipracks300_slots = tipracks300_slots
 
@@ -131,17 +178,52 @@ class LibraryStation(CovidseqBaseStation):
 
         self.drop(pipette)
 
+    def transfer_samples(self, volume, source_labware, destination_labware, mix_times=0, mix_volume=0):
+        sources = self.samples_first_row_for_labware(source_labware)
+        destinations = self.samples_first_row_for_labware(destination_labware)
+
+        pipette = self._pipette_chooser.get_pipette(volume, consider_air_gap=True)
+
+        num_transfers_per_sample = math.ceil(volume/self._pipette_chooser.get_max_volume(pipette))
+        vol_per_transfer = volume/num_transfers_per_sample
+
+        self.logger.info("We need {} transfers of {}ul for each sample".format(num_transfers_per_sample, vol_per_transfer))
+
+        for s, d in zip(sources, destinations):
+            self.pick_up(pipette)
+
+            for _ in range(num_transfers_per_sample):
+                with MoveWithSpeed(pipette,
+                                   from_point=s.bottom(self._pcr_plate_bottom_height + 2.5),
+                                   to_point=s.bottom(self._pcr_plate_bottom_height),
+                                   speed=self._slow_vertical_speed, move_close=False):
+                    pipette.aspirate(vol_per_transfer)
+
+                pipette.air_gap(self._pipette_chooser.get_air_gap(pipette))
+                pipette.dispense(self._pipette_chooser.get_air_gap(pipette), d.top())
+
+                pipette.dispense(vol_per_transfer, d.bottom(self._pcr_plate_bottom_height))
+
+                if mix_volume != 0 and mix_times != 0:
+                    mix_well(pipette, d, mix_volume, mix_times)
+
+                pipette.move_to(d.top(), speed=self._slow_vertical_speed, publish=False)
+                pipette.air_gap(self._pipette_chooser.get_air_gap(pipette))
+
+            self.drop(pipette)
+
     def body(self):
         self.pause("Load sample plate on slot {}".format(self._input_plate_slot))
         self.robot_drop_plate("SLOT{}".format(self._reagent_plate_slot), "REAGENT")
         self.distribute("EPH3", self._work_plate)
+        self.transfer_samples(8.5, self._input_plate, self._work_plate, 10, 16)
 
 
 class LibraryManualStation(LibraryStation):
     def __init__(self,
                  tipracks20_slots: Tuple[str, ...] = ("9", "6", "3"),
                  tipracks300_slots: Tuple[str, ...] = ("2",),
-                 input_plate_slot=5,
+                 input_plate_slot=4,
                  reagent_plate_slot=1,
                  work_plate_slot=7,
                  magdeck_slot=10,
