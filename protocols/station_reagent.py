67c823bbc7b2a8f16064b7176e6c564ae4d2a529
Agostino Facotti <agostino.facotti@asst-bergamoest.it>
2023-03-09 11:53:20 +0100

    ReagentStation added 24 samples in simulation

--- protocols/station_reagent.py
+++ protocols/station_reagent.py
@@ -4,7 +4,7 @@ FAKE_ROBOTMANAGER_HOST = "fakehost"
 FAKE_ROBOTMANAGER_PORT = 8080
 
 metadata = {'apiLevel': '2.7'}
-station = ReagentStation(robot_manager_host=FAKE_ROBOTMANAGER_HOST, robot_manager_port=FAKE_ROBOTMANAGER_PORT)
+station = ReagentStation(num_samples=24, robot_manager_host=FAKE_ROBOTMANAGER_HOST, robot_manager_port=FAKE_ROBOTMANAGER_PORT)
 
 def run(ctx):
     station.run(ctx)
