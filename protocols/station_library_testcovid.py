ec7c957aad18a6533113f2253c7d435ad6d0fde8
Agostino Facotti <agostino.facotti@asst-bergamoest.it>
2023-06-19 12:30:52 +0200

    Covid PCR: completed cycles to quantify DNA with qubit instead of reading fluorescence endpoint

---
+++ protocols/station_library_testcovid.py
@@ -0,0 +1,13 @@
+from covmatic_covidseq.stations.library import LibraryTestCovid
+
+FAKE_ROBOTMANAGER_HOST = "fakehost"
+FAKE_ROBOTMANAGER_PORT = 8080
+
+metadata = {'apiLevel': '2.13'}
+station = LibraryTestCovid(robot_manager_host=FAKE_ROBOTMANAGER_HOST,
+                         robot_manager_port=FAKE_ROBOTMANAGER_PORT,
+                         num_samples=40)
+
+def run(ctx):
+    station.run(ctx)
+
